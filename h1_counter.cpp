f9186424520b1e6109083560e683c3a154847cde
charles clarke <charles_b_clarke@outlook.com>
2025-09-14 10:02:29 -0700

    fulll implination?

--- h1_counter.cpp
+++ h1_counter.cpp
@@ -39,18 +39,23 @@ int main(int argc, char* argv[]) {
 	send(s, buf, sizeof(message),0);
 	int trailing_match = 0;
 	int header_count = 0;
+	int chunk_boundry = max_packet_size;
 
 	while (int size_of_recv = recv(s,buf, sizeof(buf),0)){
-		for(int i = -3; i<0; ++i){
-			int match_len = match_header(buf, size_of_recv, ++i);
-			if (match_len+trailing_match == 4) header_count+=1;
-		 }
-		for(int i = 0; i<size_of_recv-3; ++i){
-			if (match_header(buf, size_of_recv, i)==4) ++header_count;
-		}
-		for(int i = size_of_recv-3; i<size_of_recv; ++i){
-			int match_len = match_header(buf, size_of_recv, ++i);
-			if (match_len!=0) trailing_match=match_len;
+		for(int i = - 3; i< size_of_recv; ++i){
+			if (chunk_boundry < 4) {if (chunk_boundry == 0) chunk_boundry = max_packet_size;}
+			else if (i<0){
+				int match_len = match_header(buf, size_of_recv, i);
+				if (match_len+trailing_match == 4) header_count+=1;
+			}
+			else if (i< size_of_recv-3){
+				if (match_header(buf, size_of_recv, i)==4) ++header_count;
+			}
+			else{
+				int match_len = match_header(buf, size_of_recv, i);
+				if (match_len!=0) trailing_match=match_len;
+			}
+			chunk_boundry--;
 		}
 	}
 
