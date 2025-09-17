35f41f629855f62d45b3ce59a2dc55a852cd8898
charles clarke <charles_b_clarke@outlook.com>
2025-09-17 11:18:10 -0700

    nice big responce

--- h1-counter.cpp
+++ h1-counter.cpp
@@ -5,7 +5,7 @@
 int lookup_and_connect( const char *host, const char *service );
 
 int main(int argc, char* argv[]) {
-	const char *host = "www.ecst.csuchico.edu";
+	const char *host = "httpbin.org";
 	const char *port = "80";
 	int chunk_size = std::stoi(argv[1]);
 	SocketStream s(host,port,chunk_size);
@@ -15,7 +15,7 @@ int main(int argc, char* argv[]) {
 
 	char buf[chunk_size];
 
-	s << "GET /~kkredo/file.html HTTP/1.0\r\n\r\n";
+	s << "GET /bytes/100000 HTTP/1.1\r\nHost: httpbin.org\r\nConnection: close\r\n\r\n";
 
 	int header_count = 0;
 	int byte_count = 0;
